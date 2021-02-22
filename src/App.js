0d3ce6f5decd438d2f46b01491cccc4e1b4f6a9d
Mehdi <mehdi_hsb@yahoo.com>
2021-02-23 04:13:26 +0800

    Components Part 2(seperate dishes component)

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import {Navbar, NavbarBrand} from 'reactstrap';
 import Menu from './components/MenuComponent';
+import { DISHES } from './shared/dishes';
 
 
-function App() {
-  return (
-    <div className="App">
-     <Navbar dark color="primary">
-      <div className="container">
-        <NavbarBrand href="/">
-          Restorante Alfa
-        </NavbarBrand>
-      </div>
-     </Navbar>
-     <Menu/>
 
-    </div>
-  );
+// function App() {
+//   return (
+//     <div className="App">
+//      <Navbar dark color="primary">
+//       <div className="container">
+//         <NavbarBrand href="/">
+//           Restorante Alfa
+//         </NavbarBrand>
+//       </div>
+//      </Navbar>
+//      <Menu/>
+
+//     </div>
+//   );
+// }
+
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      dishes: DISHES
+    };
+  }
+  render(){
+      return (
+            <div className="App">
+             <Navbar dark color="primary">
+              <div className="container">
+                <NavbarBrand href="/">
+                  Restorante Alfa
+                </NavbarBrand>
+              </div>
+             </Navbar>
+             <Menu dishes={this.state.dishes} />
+        
+            </div>
+        );
+  }
+  
 }
 
 export default App;
